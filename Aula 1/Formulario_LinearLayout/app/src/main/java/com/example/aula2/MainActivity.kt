f033d11fd5ef39f12f081e2c44a458d11563f111
Júlio Barros <juliolopesb@gmail.com>
2023-11-06 17:34:07 -0100

    Formulario utilizando o LinearLayout

---
+++ Aula 1/Formulario_LinearLayout/app/src/main/java/com/example/aula2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aula2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
